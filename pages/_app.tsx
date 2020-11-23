414355e06f89e778939ae64105aa3b3e5bd10507
Slipromise <chengchun05@gmail.com>
2020-11-24 00:43:09 +0800

    test3

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ import { Provider } from 'react-redux';
 
 import store from '../utils/store';
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
--- reducers/index.ts
+++ reducers/index.ts
@@ -1,4 +1,4 @@
-import { combineReducers, compose } from 'redux';
+import { combineReducers } from 'redux';
 
 import matrix from './matrix';
 import setting from './setting';
